7447c2dce837293ab1a45b4bda4ce904ceab55bd
josua-yoprisyanto <josua.y8@gmail.com>
2023-02-14 09:28:36 +0700

    change button

--- src/App.js
+++ src/App.js
@@ -4,33 +4,57 @@ import heart from "./images/heart.png";
 function App() {
   const [show, setShow] = useState(false);
   const [change, setChange] = useState(false);
+  const [answer, setAnswer] = useState(false);
+
+  const handleNoButton = () => {
+    setShow(true);
+    setAnswer(false);
+  };
+
+  const handleYesButton = () => {
+    setShow(true);
+    setAnswer(true);
+  };
 
   return (
     <div className="App">
       {!show ? (
         <div className="question">
-          <span>Are You Love Me?</span>
+          <span>Do You Love Me?</span>
           <div className="button-container">
-            <button className="button no">No</button>
-            <button className="button yes" onClick={() => setShow(true)}>
+            <button className="button no" onClick={() => handleNoButton()}>
+              No
+            </button>
+            <button className="button yes" onClick={() => handleYesButton()}>
               Yes
             </button>
           </div>
         </div>
       ) : (
         <>
-          <div className="answer">
-            <span>
-              I Love{" "}
-              {!change ? (
-                <span onClick={() => setChange(true)}>Me</span>
-              ) : (
-                "You"
-              )}{" "}
-              Too
-            </span>
-            {change && <img src={heart} />}
-          </div>
+          {!answer ? (
+            <div className="answer">
+              <span>Same</span>
+            </div>
+          ) : (
+            <div className="answer">
+              <span>
+                I Love{" "}
+                {!change ? (
+                  <span
+                    onClick={() => setChange(true)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Me
+                  </span>
+                ) : (
+                  "You"
+                )}{" "}
+                Too
+              </span>
+              {change && <img src={heart} alt="love" />}
+            </div>
+          )}
         </>
       )}
     </div>
